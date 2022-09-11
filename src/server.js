679409a076c381830fc4ef1c614d7dfd008f5fe8
nicollekoetzler <nickkoetzler@gmail.com>
2022-09-11 20:42:21 -0300

    cria API cadastro

--- src/server.js
+++ src/server.js
@@ -3,18 +3,57 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
 import joi from "joi";
-import dayjs from "dayjs";
 
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGODB_URL);
-let db;
-db = mongoClient.connect().then( () => {
+    let db;
+    db = mongoClient.connect().then( () => {
     db = mongoClient.db("mywallet")
-})
+});
 
 const server = express();
 server.use(cors());
 server.use(json());
 
-server.listen(process.env.PORT, () => console.log("servidor rodando"));
+server.post("/login", async (req, res) => {
+
+    const userLogin = req.body
+});
+
+server.post("/sign-up", async (req, res) => {
+
+    const userData = req.body 
+    const userDataSchema = joi.object(
+        {
+            name: joi.string().required(),
+            email: joi.string().email().required(),
+            password: joi.string().required(),
+            passwordConfirmation: joi.string().valid(joi.ref("password")).required()
+        }
+    );
+    
+    const isBodyValid = userDataSchema.validate(userData);
+    
+    if ( isBodyValid.error ){
+        return res.sendStatus(422);
+    }
+
+    try{
+        const isUserRegistered = await db.collection("users").findOne({ email: userData.email })
+
+        if( isUserRegistered ){
+            return res.sendStatus(409)
+        }
+
+        await db.collection("users").insertOne({ name: userData.name, email: userData.email, password: userData.password})
+
+        res.sendStatus(201)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+});
+
+
+server.listen(process.env.PORT, () => console.log("servidor rodando na porta " + process.env.PORT));
